a91f08d810f39d574217e0eb94872539ff91e9f2
Bradleyh212 <bdesm052@uottawa.ca>
2025-06-26 13:19:41 -0400

    initial commit

---
+++ react-app/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Navbar, Nav, Container} from 'react-bootstrap';
+
+
+
+function Header () {
+		return (
+			<h1>Site de commerce électronique</h1>
+
+		);
+}
+
+
+export default Header;
